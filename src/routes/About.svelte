cadeb1aa615c1080651862f097e0fbab10740a51
Lewis <davanzo.lewis@gmail.com>
2021-05-11 14:46:32 -0400

    update about page

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -12,10 +12,13 @@
       <p>
         Hi! I'm a front-end web developer based in New Jersey. I have a BA in Jazz
         Performance and a Masters in Music Education. After the pandemic started I
-        began teaching myself how to code and less than a year later I'm making
+        began teaching myself how to code and now I'm making
         web apps with modern JavaScript and contributing to open-source projects
-        on GitHub! (I'm <em>technically</em> a 
-        <a href="https://github.com/sveltejs/svelte/commit/bf3535317038886b00e274b5225942a810f35158">Svelte contributor</a>.)
+        on GitHub! I'm currently a maintainer of 
+        <a href="https://github.com/pmndrs/react-spring/tree/master/packages/parallax" target="_blank" rel="noopener noreferrer">
+          @react-spring/parallax</a> (and <em>technically</em> a 
+        <a href="https://github.com/sveltejs/svelte/commit/bf3535317038886b00e274b5225942a810f35158" target="_blank" rel="noopener noreferrer">
+          Svelte contributor</a>).
       </p>
       <p>
         I like to put accessibility and responsive design at the front of my brain
@@ -42,9 +45,11 @@
           <li>HTML</li>
           <li>CSS</li>
           <li>JavaScript(ES6+)</li>
+          <li>TypeScript</li>
           <li>Node.js</li>
           <li>git</li>
-          <li>Svelte</li>
+          <li>Svelte/SvelteKit</li>
+          <li>React/Next</li>
         </ul>
       </div>
     </div>
